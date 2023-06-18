a287d4d0348638e7ecedf3699c99607c9717576e
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-06-18 19:17:44 -0300

    Criei mais uma páfina com o 5 menores países do mundo, e uma página inicial para escolher se quer saber os dados dos 5 maiores países ou dos 5 menores países. Falta estilizar a página inicial.

--- Script.js
+++ Script.js
@@ -1,3 +1,31 @@
+function biggestCountriesPage() {
+  var firstPage = document.getElementById("first-page");
+  firstPage.style.display = 'none';
+
+  var biggestCountries = document.getElementById("biggest-countries");
+  biggestCountries.style.display = 'block';
+}
+
+function smallestCountriesPage() {
+  var firstPage = document.getElementById("first-page");
+  firstPage.style.display = 'none';
+
+  var smallestCountries = document.getElementById("smallest-countries");
+  smallestCountries.style.display = 'block';
+}
+
+function backFirstPage() {
+  var firstPage = document.getElementById("first-page");
+  firstPage.style.display = 'block';
+  firstPage.style.display = 'flex';
+
+  var biggestCountries = document.getElementById("biggest-countries");
+  biggestCountries.style.display = 'none'; 
+
+  var smallestCountries = document.getElementById("smallest-countries");
+  smallestCountries.style.display = 'none'; 
+}
+
 function openModal(countryID) {
 
   var modal = document.getElementById("modal");
@@ -5,13 +33,19 @@ function openModal(countryID) {
   var countryCapital = document.getElementById("country-capital");
   var countryTerritorialArea = document.getElementById("country-territorial-area");
   var countryPopulation = document.getElementById("country-population");
+  var countryContinent = document.getElementById("country-continent");
 
   var countryData = [
-    {Name: "Rússia", Capital: "Moscou", TerritorialArea: "17,1 milhões de quilômetros quadrados", Population: "145 milhões de habitantes"},
-    {Name: "Canadá", Capital: "Ottawa", TerritorialArea: "9,98 milhões de quilômetros quadrados", Population: "38 milhões de habitantes"},
-    {Name: "China", Capital: "Pequim", TerritorialArea: "9,6 milhões de quilômetros quadrados", Population: "1,4 bilhões de habitantes"},
-    {Name: "Estados Unidos da América", Capital: "Washington", TerritorialArea: "9,5 milhões de quilômetros quadrados", Population: "331 milhões de habitantes"},
-    {Name: "Brasil", Capital: "Brasília", TerritorialArea: "8,5 milhões de quilômetros quadrados", Population: "213 milhões de habitantes"}
+    {Name: "Rússia", Capital: "Moscou", TerritorialArea: "17,1 milhões de quilômetros quadrados", Population: "145 milhões de habitantes", Continent: "Europa / Ásia"},
+    {Name: "Canadá", Capital: "Ottawa", TerritorialArea: "9,98 milhões de quilômetros quadrados", Population: "38 milhões de habitantes", Continent: "América do Norte"},
+    {Name: "China", Capital: "Pequim", TerritorialArea: "9,6 milhões de quilômetros quadrados", Population: "1,4 bilhões de habitantes", Continent: "Ásia"},
+    {Name: "Estados Unidos da América", Capital: "Washington", TerritorialArea: "9,5 milhões de quilômetros quadrados", Population: "331 milhões de habitantes", Continent: "América do Norte"},
+    {Name: "Brasil", Capital: "Brasília", TerritorialArea: "8,5 milhões de quilômetros quadrados", Population: "213 milhões de habitantes", Continent: "América do Sul"},
+    {Name: "Vaticano", Capital: "Cidade do Vaticano", TerritorialArea: "0,44 quilômetros quadrados", Population: "825 habitantes", Continent: "Europa"},
+    {Name: "Mônaco", Capital: "Monte Carlo", TerritorialArea: "2,02 quilômetros quadrados", Population: "39 mil habitantes", Continent: "Europa"},
+    {Name: "Nauru", Capital: "Yaren", TerritorialArea: "21 quilômetros quadrados", Population: "10,9 mil habitantes", Continent: "Oceania"},
+    {Name: "Tuvalu", Capital: "Funafuti", TerritorialArea: "26 quilômetros quadrados", Population: "11,6 mil habitantes", Continent: "Oceania"},
+    {Name: "São Cristóvão e Nevis", Capital: "Basseterre", TerritorialArea: "261 quilômetros quadrados", Population: "53 mil habitantes", Continent: "América do Norte"}
   ];
 
   if (countryID >= 1 && countryID <= countryData.length) {
@@ -21,22 +55,31 @@ function openModal(countryID) {
     countryCapital.textContent = country.Capital;
     countryTerritorialArea.textContent = country.TerritorialArea;
     countryPopulation.textContent = country.Population;
+    countryContinent.textContent = country.Continent;
   }
 
   modal.style.display = 'block';
 
-  var abc = document.getElementById("div-body");
-  abc.style.opacity = '0.3';
-  abc.style.filter = 'blur(10px)';
+  var biggestCountries = document.getElementById("biggest-countries");
+  biggestCountries.style.opacity = '0.3';
+  biggestCountries.style.filter = 'blur(10px)';
+
+  var smallestCountries = document.getElementById("smallest-countries");
+  smallestCountries.style.opacity = '0.3';
+  smallestCountries.style.filter = 'blur(10px)';
 }
 
 function closeModal() {
   var modal = document.getElementById("modal");
   modal.style.display = 'none';
 
-  var abc = document.getElementById("div-body");
-  abc.style.opacity = '1';
-  abc.style.filter = 'none';
+  var biggestCountries = document.getElementById("biggest-countries");
+  biggestCountries.style.opacity = '1';
+  biggestCountries.style.filter = 'none';
+
+  var smallestCountries = document.getElementById("smallest-countries");
+  smallestCountries.style.opacity = '1';
+  smallestCountries.style.filter = 'none';
 }
 
 
